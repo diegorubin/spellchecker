c68770de40a000661f17070da0adade93d2b7efc
Diego Rubin <diegorubin@ciandt.com>
2015-11-07 18:25:51 -0200

    simple spellchecker.

--- spellchecker/spellchecker.py
+++ spellchecker/spellchecker.py
@@ -1,38 +1,46 @@
 # coding: utf-8
 
 import re, collections
+import sys
+from os.path import join, abspath, dirname
+
 
 class Spellchecker():
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+    def __init__(self, dictionary_file):
+        words = self.__get_words(dictionary_file)
+        self.NWORDS = self.__train(words)
 
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    NWORDS = train(words(file('big.txt').read()))
+    def candidates(self, word):
+        candidates = self.__known([word]) or self.__known(self.__edits1(word)) or self.__known_edits2(word) or [word]
+        return candidates
 
-    def words(text):
-        return re.findall('[a-z]+', text.lower())
+    def correct(self, word):
+        candidates = self.candidates(word)
+        return max(candidates, key=self.NWORDS.get)
 
-    def train(features):
+    def __train(self, features):
         model = collections.defaultdict(lambda: 1)
         for f in features:
             model[f] += 1
         return model
 
-
-
-    def edits1(word):
+    def __edits1(self, word):
        splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
        deletes    = [a + b[1:] for a, b in splits if b]
        transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-       replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
-       inserts    = [a + c + b     for a, b in splits for c in alphabet]
+       replaces   = [a + c + b[1:] for a, b in splits for c in self.ALPHABET if b]
+       inserts    = [a + c + b     for a, b in splits for c in self.ALPHABET]
        return set(deletes + transposes + replaces + inserts)
 
-    def known_edits2(word):
-        return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+    def __known_edits2(self, word):
+        return set(e2 for e1 in __edits1(word) for e2 in __edits1(e1) if e2 in self.NWORDS)
 
-    def known(words):
-        return set(w for w in words if w in NWORDS)
+    def __known(self, words):
+        return set(w for w in words if w in self.NWORDS)
 
-    def correct(word):
-        candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
-        return max(candidates, key=NWORDS.get)
+    def __get_words(self, dictionary_file):
+        f = abspath(join(dirname(__file__), 'words', dictionary_file))
+        words = file(f).read()
+        return re.findall('[\w\-]+', words)
 
---
+++ tests/test_spellchecker.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import unittest
+
+try:
+    from spellchecker.spellchecker import Spellchecker
+except ImportError:
+    import sys
+    from os.path import join, abspath, dirname
+    parentpath = abspath(join(dirname(__file__), '..'))
+    sys.path.append(parentpath)
+    from spellchecker.spellchecker import Spellchecker
+
+
+class TestSpellchecker(unittest.TestCase):
+    def setUp(self):
+        self.spellchecker = Spellchecker('pt_BR.wl')
+
+    def test_correct(self):
+        self.assertEqual('abacaxi', self.spellchecker.correct('abacahi'))
+
+    def test_candidates(self):
+        self.assertEqual(set(['abacaxi']), self.spellchecker.candidates('abacahi'))
+
+unittest.main()
+
