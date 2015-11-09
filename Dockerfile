fc204e1fff70580cdec624f0f4ba6e40794570d6
Diego Rubin <diegorubin@ciandt.com>
2015-11-09 21:25:29 -0200

    Dockerfile and fabric script to deploy.

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM fedora:23
+MAINTAINER rubin.diego@gmail.com
+
+# Copy application
+RUN mkdir /app
+ADD bin /app/bin
+ADD spellchecker /app/spellchecker
+RUN ln -s /app/bin/spellchecker-server /usr/bin/spellchecker-server
+
+# System dependences 
+# install cyclone system dependences
+RUN dnf install -y libffi-devel
+RUN dnf install -y gcc
+RUN dnf install -y rpm-build
+
+# install python2
+RUN dnf install -y python-pip
+RUN dnf install -y python-devel
+RUN dnf install -y openssl-devel 
+
+# Python dependences
+RUN pip install cyclone
+RUN pip install redis
+
+WORKDIR /app/spellchecker
+
+EXPOSE 8084
+
---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import run
+
+CONTAINER_NAME='spellchecker-server'
+COMMAND='/app/bin/spellchecker-server'
+
+def start_container():
+    run("docker run --net=host -d --name %s diegorubin/spellchecker %s"%(CONTAINER_NAME, COMMAND))
+
+def remove_container():
+    run("docker rm %s || echo 'not exists'"%(CONTAINER_NAME))
+
+def stop_container():
+    run("docker stop %s || echo 'not running'"%(CONTAINER_NAME))
+
+def update_docker_image():
+    run("docker pull diegorubin/spellchecker")
+
+def update_spellchecker():
+    stop_container()
+    remove_container()
+    update_docker_image()
+    start_container()
+
+
