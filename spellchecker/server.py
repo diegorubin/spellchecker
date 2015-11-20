d71d8df82b580abd582cea426160da171aa074eb
Diego Rubin <rubin.diego@gmail.com>
2015-11-20 11:04:36 -0200

    support words with accents.

--- spellchecker/server.py
+++ spellchecker/server.py
@@ -4,18 +4,22 @@ import cyclone.web
 import re
 from spellchecker import Spellchecker
 
-spellchecker = Spellchecker('pt_BR.wl')
+spellchecker = Spellchecker('pt_BR.list')
 
 class CheckWordHandler(cyclone.web.RequestHandler):
     def get(self, word):
         self.set_header("Content-Type", "application/json")
-        self.write(spellchecker.verify_and_analyze(word.decode('utf-8')))
+        self.write(spellchecker.verify_and_analyze(word))
 
 class CheckTextHandler(cyclone.web.RequestHandler):
     def post(self):
         text = self.request.body.decode('utf-8')
         self.set_header("Content-Type", "application/json")
         self.write(spellchecker.verify_and_analyze_text(text))
+
+class WordListHandler(cyclone.web.RequestHandler):
+    def post(self):
+        self.set_header("Content-Type", "application/json")
 
 class ApplicationServer(cyclone.web.Application):
     def __init__(self):
--- spellchecker/spellchecker.py
+++ spellchecker/spellchecker.py
@@ -4,9 +4,10 @@ import re, collections
 import sys
 from os.path import join, abspath, dirname
 
+import pdb
 
 class Spellchecker():
-    ALPHABET = unicode('abcdefghijklmnopqrstuvwxyz')
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyzãâàáéêíõóôúç'.decode('utf-8')
     def __init__(self, dictionary_file):
         words = self.__get_words(dictionary_file)
         self.NWORDS = self.__train(words)
@@ -16,7 +17,6 @@ class Spellchecker():
         return candidates
 
     def correct(self, word):
-        print 'checking word %s'%(word)
         candidates = self.candidates(word)
         return max(candidates, key=self.NWORDS.get)
 
@@ -33,6 +33,7 @@ class Spellchecker():
         return result
 
     def verify_and_analyze(self, word):
+        print 'checking word %s'%(word)
         correct = self.correct(word)
         candidates = self.candidates(word)
 
@@ -49,11 +50,12 @@ class Spellchecker():
         print 'trainning spellchecker'
         model = collections.defaultdict(lambda: 1)
         for f in features:
-            model[f] += 1
+            model[unicode(f, 'utf-8')] += 1
         return model
 
     def __edits1(self, word):
        splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+
        deletes    = [a + b[1:] for a, b in splits if b]
        transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
        replaces   = [a + c + b[1:] for a, b in splits for c in self.ALPHABET if b]
@@ -69,5 +71,5 @@ class Spellchecker():
     def __get_words(self, dictionary_file):
         f = abspath(join(dirname(__file__), 'words', dictionary_file))
         words = file(f).read()
-        return re.findall('[\w\-]+', words, re.UNICODE)
+        return words.split("\n")
 
