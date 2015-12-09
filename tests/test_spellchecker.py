530941016dd0866790633893b2b78bfa30062d5f
Diego Rubin <rubin.diego@gmail.com>
2015-12-09 21:17:14 -0200

    fixes testes.

--- tests/test_spellchecker.py
+++ tests/test_spellchecker.py
@@ -4,17 +4,20 @@ import unittest
 
 try:
     from spellchecker.spellchecker import Spellchecker
+    from spellchecker.word_list import Word
 except ImportError:
     import sys
     from os.path import join, abspath, dirname
     parentpath = abspath(join(dirname(__file__), '..'))
     sys.path.append(parentpath)
+    from spellchecker.word_list import Word
     from spellchecker.spellchecker import Spellchecker
 
 
 class TestSpellchecker(unittest.TestCase):
     def setUp(self):
-        self.spellchecker = Spellchecker('pt_BR.wl')
+        self.spellchecker = Spellchecker('pt_BR.list')
+        word = Word('abacaxi', 'TEST')
 
     def test_correct(self):
         self.assertEqual('abacaxi', self.spellchecker.correct('abacahi'))
